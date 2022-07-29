336ffbc766753b82a8a533f2ca1a7fdb4f67ac81
Lukas Danckwerth <lukas.danckwerth@gmx.de>
2022-07-29 18:48:47 +0200

    [TASK] Update package.json

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import "./map.scss";
 export * from "./map.js";
-export * from "lotivis-chart";
-export * from "lotivis-data";
-export * from "lotivis-datasets";
-export * from "lotivis-colors";
-export * from "lotivis-geojson";
+// export * from "lotivis-chart";
+// export * from "lotivis-data";
+// export * from "lotivis-datasets";
+// export * from "lotivis-colors";
+// export * from "lotivis-geojson";
